8f3414be46e6d23c14c4b00d3e9ac127cf14af78
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-08-28 23:54:57 +0800

    rest v1.0.5

--- restapiproject/settings.py
+++ restapiproject/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'restapiproject/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
