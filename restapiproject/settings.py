dd172c4c5141574a88daac43ff65fa26bdabaf45
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-08-29 09:20:24 +0800

    fixing static

--- restapiproject/settings.py
+++ restapiproject/settings.py
@@ -112,10 +112,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_ROOT = 'restapiproject/static'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
