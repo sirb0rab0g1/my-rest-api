b7080523f9e0c0325fd43bafd4baf6cc5b3d7639
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-08-29 15:36:35 +0800

    adding group

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from quickstart.serializers import UserSerializer
+from quickstart.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,10 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- restapiproject/urls.py
+++ restapiproject/urls.py
@@ -18,8 +18,12 @@ from rest_framework import routers
 from quickstart import views
 from django.contrib import admin
 
-router = routers.DefaultRouter()
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
