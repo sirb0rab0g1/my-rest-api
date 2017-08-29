47cad1d5c60a8b864cc061ff267cf027c9154bb0
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-08-29 10:26:41 +0800

    adding path on static root

--- restapiproject/settings.py
+++ restapiproject/settings.py
@@ -112,5 +112,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = 'restapiproject/restapiproject/static'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
